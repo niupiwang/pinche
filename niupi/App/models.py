16243f19b13b04301c95ec7fd79648719ed3d08d
ymicoo <1308689064@qq.com>
2019-09-17 21:42:56 +0800

    a

--- niupi/App/models.py
+++ niupi/App/models.py
@@ -7,61 +7,89 @@ from django.db import models
 
 #1用户表
 class User(models.Model):
-    # uid = models.AutoField(primary_key=True, )
+    uid = models.AutoField(primary_key=True)
     username = models.CharField(max_length=100, verbose_name='用户名')
     password = models.CharField(max_length=128, verbose_name='密码')
-    real_name = models.CharField(null=True, max_length=20, verbose_name='真实姓名')
-    id_num = models.IntegerField(null=True, unique=True, verbose_name='身份证号')
-    birthday = models.DateTimeField(null=True, verbose_name='生日')
-    address = models.CharField(null=True, max_length=200, verbose_name='地址')
-    age = models.IntegerField(null=True, verbose_name='年龄')
-    g_type = ((0, '保密'), (1, '男'), (2, '女'))
-    gender = models.IntegerField(null=True, default=0, choices=g_type, verbose_name='性别')
+    phone = models.CharField(max_length=64,verbose_name='手机号')
+    is_active = models.IntegerField(default=1) # 1代表激活 0代表锁定
+    type = models.IntegerField(default=0) # 0为普通用户 1为司机 2为管理员用户
 
     class Meta:
         db_table = 'user'
         verbose_name_plural = '用户表'
 
+#2用户详情
+class Userdetail(models.Model):
+    eid = models.AutoField(primary_key=True)
+    is_vip = models.IntegerField(default=0) # 0否 1是
+    real_name = models.CharField(null=True, max_length=20, verbose_name='真实姓名')
+    id_num = models.IntegerField(null=True, unique=True, verbose_name='身份证号')
+    birthday = models.DateTimeField(null=True, verbose_name='生日')
+    address = models.CharField(null=True, max_length=200, verbose_name='地址')
+    age = models.IntegerField(null=True, verbose_name='年龄')
+    pic = models.CharField(null=True,max_length=128) # 头像
+    g_type = ((0, '保密'), (1, '男'), (2, '女'))
+    gender = models.IntegerField(null=True, default=0, choices=g_type, verbose_name='性别')
+    car_num = models.CharField(max_length=10, unique=True, verbose_name='车牌号码',null=True)
+    user_uid = models.ForeignKey(User,models.DO_NOTHING,db_column='uid',blank=True,null=False)
+    class Meta:
+        db_table = 'userdetail'
+        verbose_name_plural = '用户详情'
 
-#2汽车信息表
+
+
+#3汽车信息表
 class Bus(models.Model):
-    # num = models.CharField(max_length=10, verbose_name='列车编号')
-    # b_type = ((0, '经济型'), (1, '豪华舒适型'), (2, '临时加车'))
-    # bus_type = models.IntegerField(default=0, choices=b_type, verbose_name='汽车类型')
-    driver = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='司机')
-    car_type = models.CharField(max_length=100, verbose_name='车型描述（品牌，名称，座位数）')
-    plate_num = models.CharField(max_length=10, unique=True, verbose_name='车牌号码')
+    bid = models.AutoField(primary_key=True)
+    num = models.CharField(max_length=10, verbose_name='列车编号')
+    bus_num = models.CharField(max_length=10, unique=True, verbose_name='车牌号码')
     start_time = models.DateTimeField(verbose_name='起始时间')
     end_time = models.DateTimeField(verbose_name='到达时间')
-    time = models.DateTimeField(verbose_name='耗时')
-    city_list = ((0, '未选择'), (1, '石家庄'), (2, '秦皇岛'))
-    start_city = models.IntegerField(default=0, choices=city_list, verbose_name='起始城市')
-    end_city = models.IntegerField(default=0, choices=city_list, verbose_name='到达城市')
-    # station_list = ((0, '未选择'), (1, '石家庄汽车总站'), (2, '秦皇岛长途客运站'))
-    # start_station = models.IntegerField(null=True, default=0, choices=station_list, verbose_name='始发车站')
-    # end_station = models.IntegerField(null=True, default=0, choices=station_list, verbose_name='到达车站')
-    price = models.IntegerField(null=True, verbose_name='价格')
-    ticket = models.IntegerField(null=True, verbose_name='总票数')
-    standby_ticket = models.IntegerField(null=True, verbose_name='余票')
-
+    start_city = models.CharField(max_length=128, verbose_name='起始城市')
+    end_city = models.CharField(max_length=128, verbose_name='到达城市')
+    start_station = models.CharField(max_length=128, verbose_name='始发车站')
+    end_station = models.CharField(max_length=128, verbose_name='到达车站')
+    price = models.IntegerField(verbose_name='价格')
+    ticket = models.IntegerField(verbose_name='总票数')
+    standby_ticket = models.IntegerField(verbose_name='余票')
     class Meta:
-        db_table = 'ticket'
+        db_table = 'bus'
         verbose_name_plural = '车次表'
 
 
-#3我的订单表
-class MyList(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
-    driver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='driver')
-    bus = models.ForeignKey(Bus, on_delete=models.CASCADE,)
-    # start_time = models.DateTimeField(default=datetime.now(), null=True)
+#4汽车信息表
+class Car(models.Model):
+    cid = models.AutoField(primary_key=True)
+
+    car_type = models.CharField(max_length=100, verbose_name='车型描述（品牌，名称，座位数）')
+    start_time = models.DateTimeField(verbose_name='出发时间')
+    start_city = models.CharField(max_length=128, verbose_name='起始城市')
+    end_city = models.CharField(max_length=128, verbose_name='到达城市')
+    price = models.IntegerField(null=True, verbose_name='价格')
+    seats = models.IntegerField(null=True, verbose_name='总座位数')
+    left_seats = models.IntegerField(null=True, verbose_name='剩余座位')
+    car_uid = models.ForeignKey(User, models.DO_NOTHING, db_column='uid', blank=True, null=True)
+
+    class Meta:
+        db_table = 'car'
+        verbose_name_plural = '拼车表'
+
+
+#5我的订单表
+class List(models.Model):
+    lid = models.AutoField(primary_key=True)
+    num_list = models.CharField(max_length=128)
     s_list = ((0, '未完成'), (1, '已取消'), (2, '已完成'))
     status = models.IntegerField(default=0, choices=s_list, verbose_name='订单状态')
     list_time = models.DateTimeField(default=datetime.now, verbose_name='下单时间')
-    # list_type = models.IntegerField(default=0, choices=((0, '我要订车'), (1, '我要拉活')), )
+
+
+    # waijian
+    list_uid = models.ForeignKey(User, models.DO_NOTHING, db_column='uid', blank=True, null=False)
+    list = models.CharField(max_length=128, null=False) # 订单的车辆列表
 
     class Meta:
-        db_table = 'my_list'
+        db_table = 'list'
         verbose_name_plural = '订单表'
 
 
--- niupi/App/views.py
+++ niupi/App/views.py
@@ -1,16 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.urls import reverse
 from django.views import View
+
+from App.models import Bus
 
 
 def index(request):
-    return render(request,'index.html')
+    if request.method == 'POST':
+        start_city = request.POST.get('from')
+        end_city = request.POST.get('go')
+        buses = Bus.objects.filter(start_city=start_city,end_city=end_city).order_by('start_time')
+        if start_city and end_city:
 
+            return render(request, 'app/select.html',locals())
+    return render(request,'app/index.html')
+
+def select(request):
+    if request.method == 'POST':
+        start_city = request.POST.get('from')
+        end_city = request.POST.get('go')
+        print(start_city)
+    # cars = Bus.objects.filter(start_city=start_city,end_city=end_city)
+    return render(request,'app/select.html')
+
+def buy(request):
+    return render(request,'app/buy.html')
 
 def userinfo(request):
-    return render(request,'userinfo.html')
+    return render(request,'app/userinfo.html')
+
+
 
 
 class UserView(View):
-    pass
+    def get(self,request):
+        pass
+    def post(self,request):
+        pass
+
+
